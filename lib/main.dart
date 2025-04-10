c9c1a61db42fd1d7ca27b160cc0a9458e8a1b705
ahmed ibrahim mousa <ahmed0128382@gmail.com>
2025-04-10 09:16:03 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:storeapp/views/note_view.dart';
+
+void main() {
+  runApp(const NoteApp());
+}
+
+class NoteApp extends StatelessWidget {
+  const NoteApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(brightness: Brightness.dark),
+      home: const NoteView(),
+    );
+  }
+}
